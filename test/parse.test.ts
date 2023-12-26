6e29ca53814c9c9e91218df46b03746515510641
Omochice <mochice.mls.ntl@gmail.com>
2023-12-26 22:04:01 +0900

    test: call `parse`

--- test/parse.test.ts
+++ test/parse.test.ts
@@ -1,7 +1,5 @@
-import {
-  assertEquals,
-  assertObjectMatch,
-} from "https://deno.land/std@0.210.0/assert/mod.ts";
+import { assertEquals } from "https://deno.land/std@0.210.0/assert/mod.ts";
+import { parse } from "../mod.ts";
 
 Deno.test("parse()", async (t) => {
   await t.step("should parse array of string", async (t) => {
@@ -16,12 +14,12 @@ Deno.test("parse()", async (t) => {
     });
     await t.step("should parse multiple character", () => {
       assertEquals(parse('("abc" "def")'), ["abc", "def"]);
-    })
+    });
     await t.step("should parse if quote single", () => {
       assertEquals(parse("('a' 'b')"), ["a", "b"]);
-    })
+    });
     await t.step("should parse if include cr", () => {
       assertEquals(parse("('a' \n 'b')"), ["a", "b"]);
-    })
+    });
   });
 });
